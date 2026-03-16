b30cd00f527e2cf369aad5f9325e2b8c0fcd19ab
marcturu <marcturu80@gmail.com>
2026-03-16 19:18:40 +0100

    Fetching API

--- PEC1_Ej2_2/script.js
+++ PEC1_Ej2_2/script.js
@@ -1,9 +1,37 @@
+var currencyOne = document.getElementById('currency-one');
+var currencyTwo = document.getElementById('currency-two');
+var amountOne = document.getElementById('amount-one');
+var amountTwo = document.getElementById('amount-two');
+var rate = document.getElementById('rate');
+
 function calculateExchange() {
+  fetch('https://api.exchangerate-api.com/v4/latest/' + currencyOne.value)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      var dataRate = data.rates[currencyTwo.value];
+      amountTwo.value = (amountOne.value * dataRate).toFixed(2);
+
+      rate.innerText = '1 ' + currencyOne.value  + ' = ' + dataRate + ' ' + currencyTwo.value;
+    })
+}
+
+function swapCurrencies() {
+  var temp = currencyOne.value;
+  currencyOne.value = currencyTwo.value;
+  currencyTwo.value = temp;
+
+  calculateExchange();
 }
 
 /* ----- Event listeners ----- */
 
-document.getElementById('currency-one').addEventListener('change', calculateExchange);
-document.getElementById('amount-one').addEventListener('input', calculateExchange);
-document.getElementById('currency-two').addEventListener('change', calculateExchange);
-document.getElementById('amount-two').addEventListener('input', calculateExchange);
+currencyOne.addEventListener('change', calculateExchange);
+amountOne.addEventListener('input', calculateExchange);
+currencyTwo.addEventListener('change', calculateExchange);
+amountTwo.addEventListener('input', calculateExchange);
+
+document.getElementById('swap').addEventListener('click', swapCurrencies);
+
+calculateExchange();
